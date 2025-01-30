b7267671d665637a4b915ab0ec56da209f0e54aa
carlosrocha <carlosrocha@gmail.com>
2025-01-29 22:43:29 -0600

    proyecto redes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AsideComponent } from './aside/aside.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet ,AsideComponent,FooterComponent,HeaderComponent,ProfileComponent],
+
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'redes';
-}
+export class AppComponent { title = 'redes';}
+
